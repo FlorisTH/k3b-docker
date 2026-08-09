210fcb482d3dadfb651564293d12aada1ec7f8fa
FlorisTH <121720811+FlorisTH@users.noreply.github.com>
2026-08-09 17:14:32 +0200

    Add udisks2 directories and set ownership

--- rootfs/etc/cont-init.d/12-dbus-dir.sh
+++ rootfs/etc/cont-init.d/12-dbus-dir.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 mkdir -p /run/dbus
 chown "${USER_ID:-1000}:${GROUP_ID:-1000}" /run/dbus
+
+mkdir -p /run/udisks2
+chown "${USER_ID:-1000}:${GROUP_ID:-1000}" /run/udisks2
+
+mkdir -p /var/lib/udisks2
+chown "${USER_ID:-1000}:${GROUP_ID:-1000}" /var/lib/udisks2
